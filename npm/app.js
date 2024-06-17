c8c14794dce5ad383441da100aef87ee3ac68beb
dandidamanik <dandidamanik210497@gmail.com>
2024-06-17 09:20:58 +0700

    belajar membuat npm dan menjalankannya

---
+++ npm/app.js
@@ -0,0 +1,7 @@
+//uninstall npm , npm uninstall nama npm, cth: npm uninstall validator
+//install npm, nmp i nama npm
+
+const validator = require('validator');
+
+console.log(validator.isEmail('dandi@gmail.com')); //cth npm validator
+console.log(validator.isMobilePhone('082321557307','id-ID'));
